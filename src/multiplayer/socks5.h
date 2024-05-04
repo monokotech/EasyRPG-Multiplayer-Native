6f5633507499be3ece79b813585666d705f696ae
MonokoTech <137059552+monokotech@users.noreply.github.com>
2024-05-04 15:19:39 +0800

    EPMP: Add socket read timeout & Re-add SOCKS5 (+ Recv/Send Raw buf)

--- src/multiplayer/socks5.h
+++ src/multiplayer/socks5.h
@@ -1,5 +1,7 @@
+#ifndef EP_SOCKS5_H
+#define EP_SOCKS5_H
+
 #include <vector>
-#include <functional>
 
 // Defaults
 enum class SOCKS5_DEFAULTS : std::uint8_t {
@@ -34,28 +36,27 @@ enum class SOCKS5_CLIENT_CONNCMD : std::uint8_t {
 	SS_UDP_PORT = 0x03
 };
 
-struct SOCKS5_Initializer {
-	std::function<ssize_t(void*, const size_t&)> read;
-	std::function<ssize_t(const void*, const size_t&)> write;
-
-	int SendGreeting() const {
+struct SOCKS5 {
+	static const std::vector<char> GetGreeting() {
 		std::vector<char> client_greeting_msg = {
 			static_cast<char>(SOCKS5_DEFAULTS::SS_VERSION),
 			static_cast<char>(SOCKS5_DEFAULTS::SS_SUPPORT_AUTH),
 			static_cast<char>(SOCKS5_AUTH_TYPES::SS_NOAUTH)
 		};
-		write(client_greeting_msg.data(), client_greeting_msg.size());
+		return std::move(client_greeting_msg);
+	}
 
-		std::vector<char> server_choice(2);
-		int read_ret = read(server_choice.data(), server_choice.size());
-		if(server_choice.at(0) == 0x05 && server_choice.at(1) == static_cast<char>(SOCKS5_AUTH_TYPES::SS_NOAUTH)) {
+	static int CheckGreeting(const std::vector<char>& server_choice) {
+		if(server_choice.size() == 2 && server_choice.at(0) == 0x05 &&
+				server_choice.at(1) == static_cast<char>(SOCKS5_AUTH_TYPES::SS_NOAUTH)) {
 			return 0;
 		} else {
 			return -1;
 		}
 	}
 
-	int SendConnectionRequest(const std::string& destination_addr, const std::uint16_t& destination_port) {
+	static const std::vector<char> GetConnectionRequest(const std::string_view destination_addr,
+			const uint16_t destination_port) {
 		std::vector<char> client_conn_request = {
 			static_cast<char>(SOCKS5_DEFAULTS::SS_VERSION),
 			static_cast<char>(SOCKS5_CLIENT_CONNCMD::SS_TCP_IP_STREAM),
@@ -67,11 +68,12 @@ struct SOCKS5_Initializer {
 			client_conn_request.push_back(destination_addr.at(i));
 		client_conn_request.push_back(static_cast<char>(destination_port >> 8));
 		client_conn_request.push_back(static_cast<char>(destination_port));
-		write(client_conn_request.data(), client_conn_request.size());
+		return std::move(client_conn_request);
+	}
 
-		std::vector<char> server_responce(client_conn_request.size());
-		int read_ret = read(server_responce.data(), server_responce.size());
-		if(server_responce.at(0) == 0x05 && server_responce.at(1) == 0x00) {
+	static int CheckConnectionRequest(const std::vector<char>& server_responce) {
+		if(server_responce.size() >= 2 &&
+				server_responce.at(0) == 0x05 && server_responce.at(1) == 0x00) {
 			return 0;
 		} else {
 			return -1;
@@ -79,26 +81,4 @@ struct SOCKS5_Initializer {
 	}
 };
 
-//int SOCKS5_AUTH::client_auth_handler() const {
-//	std::vector<char> client_auth_request = {
-//		static_cast<char>(SOCKS5_DEFAULTS::VER_USERPASS),
-//		static_cast<char>(this->_proxy_username.length())
-//	};
-//	for(std::size_t i{}; i < this->_proxy_username.length(); i++){
-//		client_auth_request.push_back(this->_proxy_username.at(i));
-//	}
-//
-//	client_auth_request.push_back(static_cast<char>(this->_proxy_password.length()));
-//	for(std::size_t i{}; i < this->_proxy_password.length(); i++){
-//		client_auth_request.push_back(this->_proxy_password.at(i));
-//	}
-//	int write_ret = SOCKS5::write_data(this->_client_net_fd, client_auth_request.data(), client_auth_request.size(), 0);
-//	std::vector<char> server_auth_responce(2);
-//
-//	int read_ret = SOCKS5::read_data(this->_client_net_fd, server_auth_responce.data(), server_auth_responce.size(), 0);
-//	if(server_auth_responce.at(0) == static_cast<char>(SOCKS5_DEFAULTS::VER_USERPASS) && server_auth_responce.at(1) == 0x00){
-//		return 0;
-//	}else{
-//		return -1;
-//	}
-//}
+#endif
